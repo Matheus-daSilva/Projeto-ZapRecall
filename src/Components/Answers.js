ea6b291df9d1d2b941c0c4f217731c832e42669a
Matheus-daSilva <matheus2001silva@live.com>
2022-03-24 14:49:39 -0300

    fix: Flashcards

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -1,46 +1,32 @@
 import { useState } from "react";
 
 export default function Answers(props) {
-    const {ID, setFinished, finished} = props;
+    const {questions, ID, setFinished, finished} = props;
     const [selected, setSelected] = useState(true);
-    const [state, setState] = useState("true");
-    const questions = ["Pergunta 1", "Pergunta 2", "Pergunta 3", "Pergunta 4", "Pergunta 5", "Pergunta 6", "Pergunta 7", "Pergunta 8"];
-    const answers = [
-        "Uma extensão de linguagem do JavaScript",
-        "Uma biblioteca JavaScript para construção de interfaces",
-        "Letra maiúscula",
-        "Expressões",
-        "Interagindo com a DOM para colocar componentes React na mesma",
-        "Gerenciar os pacotes necessários e suas dependências",
-        "Passar diferentes informações para componentes",
-        "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
-    ];
-    if (state == "true") {
+    const [state, setState] = useState("");
+    if (state == "") {
         return (
             <div className="question">
-                <p>{answers[ID]}</p>
+                <p>{questions[ID].answer}</p>
                 <div className="answer-buttons">
-                    <div onClick={() => setState("red")}>
                         <button className="red" onClick={() => {
                             setSelected(false)
                             setFinished(finished + 1)
+                            setState("red")
                         }
                         }>Não lembrei</button>
-                    </div>
-                    <div onClick={() => setState("orange")}>
                         <button className="orange" onClick={() => {
                             setSelected(false)
                             setFinished(finished + 1)
+                            setState("orange")
                         }
                         }>Quase não lembrei</button>
-                    </div>
-                    <div onClick={() => setState("green")}>
                         <button className="green" onClick={() => {
                             setSelected(false)
                             setFinished(finished + 1)
+                            setState("green")
                         }
                         }>Zap</button>
-                    </div>
                 </div>
             </div>
         )
@@ -50,7 +36,7 @@ export default function Answers(props) {
         if (state == "red") {
             return (
                 <div className="card card-red">
-                    <p>{questions[ID]}</p>
+                    <p>Pergunta {ID + 1}</p>
                     <ion-icon name="close-circle"></ion-icon>
                 </div>
             )
@@ -58,7 +44,7 @@ export default function Answers(props) {
         if (state == "orange") {
             return (
                 <div className="card card-orange">
-                    <p>{questions[ID]}</p>
+                    <p>Pergunta {ID + 1}</p>
                     <ion-icon name="help-circle"></ion-icon>
                 </div>
             )
@@ -66,7 +52,7 @@ export default function Answers(props) {
         if (state == "green") {
             return (
                 <div className="card card-green">
-                    <p>{questions[ID]}</p>
+                    <p>Pergunta {ID + 1}</p>
                     <ion-icon name="checkmark-circle"></ion-icon>
                 </div>
             )
--- src/Components/Flashcards.js
+++ src/Components/Flashcards.js
@@ -4,41 +4,18 @@ import Footer from "./Footer";
 
 export default function Flashcards() {
     const [finished, setFinished] = useState(0);
+    const [mix, setMix] = useState([]);
     const questions = [
-        {
-            question: "O que é JSX?",
-            answer: "Uma extensão de linguagem do JavaScript"
-        },
-
-        {
-            question: "O React é__",
-            answer: "Uma biblioteca JavaScript para construção de interfaces"
-        },
-        {
-            question: "Componentes devem iniciar com__",
-            answer: "Letra maiúscula"
-        },
-        {
-            question: "Podemos colocar __ dentro do JSX",
-            answer: "Expressões"
-        },
-        {
-            question: "O ReactDOM nos ajuda__",
-            answer: "Interagindo com a DOM para colocar componentes React na mesma"
-        },
-        {
-            question: "Usamos o npm para__",
-            answer: "Gerenciar os pacotes necessários e suas dependências"
-        },
-        {
-            question: "Usamos estado (state) para__",
-            answer: "Interagindo com a DOM para colocar componentes React na mesma"
-        },
-        {
-            question: "Usamos props para__",
-            answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
-        },
+        { question: "O que é JSX?", answer: "Uma extensão de linguagem do JavaScript" },
+        { question: "O React é__", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+        { question: "Componentes devem iniciar com__", answer: "Letra maiúscula" },
+        { question: "Podemos colocar __ dentro do JSX", answer: "Expressões" },
+        { question: "O ReactDOM nos ajuda__", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
+        { question: "Usamos o npm para__", answer: "Gerenciar os pacotes necessários e suas dependências" },
+        { question: "Usamos estado (state) para__", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" },
+        { question: "Usamos props para__", answer: "Passar diferentes informações para componentes " },
     ];
+    mixCards();
     return (
         <>
             <header>
@@ -48,11 +25,17 @@ export default function Flashcards() {
             <main>
 
                 {questions.map((question) => {
-                    return <Card ID={questions.indexOf(question)} finished={finished} setFinished={setFinished} questions={questions}/>
+                    return <Card ID={questions.indexOf(question)} finished={finished} setFinished={setFinished} questions={mix} />
                 })}
 
             </main>
             <Footer finished={finished} />
         </>
     )
+    function mixCards() {
+        if (mix.length == 0) {
+            questions.sort((a,b) => Math.random() - 0.5);
+            setMix([...questions]);
+        }
+    }
 }
